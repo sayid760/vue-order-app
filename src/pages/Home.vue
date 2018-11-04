ca8d0765bff012287ecd1f7fe861ac133b279735
sayid <707207578@qq.com>
2018-11-04 23:25:26 +0800

    https://gitee.com/fanfan760/vue-order-app.git

---
+++ src/pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    home
+
+  </div>
+</template>
+
+<script>
+// import {post} from '@/utils/api'
+import axios from 'axios'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      // post('http://api.douban.com/v2/movie/top250',{ start:25,count:2,}).then(res=>{
+      //   console.log(res)
+      // })
+      axios.get('/admin/manage').then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
---
+++ src/utils/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+// export const get = (url) => {
+//   return fetch(url, {
+//     method: 'get', // 获取的方法
+//     credentials: 'include', // 跨域携带cookie
+//     headers: {
+//       accept: 'application/json'
+//     }
+//   }).then(res => res.json())
+// }
+
+// export const post = (url, data) => {
+//   return fetch(url, {
+//     method: 'post',
+//     credentials: 'include',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       accept: 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   }).then(res => res.json())
+// }
+
+export const get = (url, data) => {
+  return axios.get(url, {
+    params: data
+  }).then(res => res.json())
+}
+
+export const post = (url, data) => {
+  return axios.post(url, data)
+}
